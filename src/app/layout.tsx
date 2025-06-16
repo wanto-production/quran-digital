701e7f8962b16dbba050d242dd0a61ab995618dc
wanto-production <ikwansatria3974@gmail.com>
2025-06-16 16:45:52 +0700

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,35 +5,35 @@ import Header from '@/components/header'
 import Providers from '@/components/providers'
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 })
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
-  title: {
-    default: 'Quran Digital',
-    template: '%s | Quran Digital',
-  },
+	title: {
+		default: 'Quran Digital',
+		template: '%s | Quran Digital',
+	},
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>
-          <Header />
-          {children}
-        </Providers>
-      </body>
-    </html>
-  )
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang="en">
+
+			<body className={` ${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<Providers>
+					<Header />
+					{children}
+				</Providers>
+			</body>
+
+		</html>
+	)
 }
